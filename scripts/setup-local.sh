2007066183b310e863b997f5be79c06296ee0943
Alejandro Visiedo <avisiedo@redhat.com>
2022-04-20 16:10:56 +0200

    Add appsre and app-interface repos

--- scripts/setup-local.sh
+++ scripts/setup-local.sh
@@ -8,19 +8,20 @@ set -e
 # shellcheck disable=SC1091
 [ ! -e scripts/private.inc.sh ] || source "scripts/private.inc.sh"
 [ ! -e scripts/variables.inc.sh ] || source "scripts/variables.inc.sh"
+source "scripts/helper.inc.sh"
 
 function validation {
   if [ "${quayio_user}" == "" ]; then
-      echo "ERROR:quayio_user can not be empty. Set variable quayio_user to the username at quayio"
+      msg_error "The 'quayio_user' variable can not be empty. Set variable quayio_user to the username at quayio"
       exit 1
   fi
 
   if [ "${quayio_pull_secret_path}" == "" ] \
     || [ ! -e "${quayio_pull_secret_path}" ]; then
-    echo "ERROR:quayio_pull_secret_path is empty or the path does not exist"
-    echo "ERROR:Set quayio_pull_secreto pointing out to yours; by default it try 'quayio-pull-secret.yaml'"
-    echo "INFO:Retrieve the information as indicated here:"
-    echo "INFO:https://consoledot.pages.redhat.com/docs/dev/getting-started/local/environment.html#_get_your_quay_pull_secret"
+    msg_error "The 'quayio_pull_secret_path' variable is empty or the path does not exist"
+    msg_error "Set the 'quayio_pull_secret_path' to pointing out to yours; by default it try 'quayio-pull-secret.yaml'"
+    msg_info "Retrieve the information as indicated here:"
+    msg_info "https://consoledot.pages.redhat.com/docs/dev/getting-started/local/environment.html#_get_your_quay_pull_secret"
     exit 1
   fi
 }
@@ -33,14 +34,14 @@ function create_dirs {
 function install_git {
   # Install git
   command -v git &>/dev/null || {
-    echo "> Installing git"
+    msg_info "> Installing git"
     sudo dnf install -y git
   }
 }
 
 function install_virsh {
   command -v virsh &>/dev/null || {
-    echo "> Installing virtualization packages"
+    msg_info "> Installing virtualization packages"
     sudo dnf install -y @virtualization
     sudo usermod -a -G libvirt $(whoami)
     newgrp libvirt
@@ -51,8 +52,10 @@ function install_virsh {
 function install_aws_cli {
   [ -e .cache ] || mkdir .cache
   [ -e .cache/awscliv2.zip ] || curl -sL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o ".cache/awscliv2.zip"
-  [ -e .cache/aws ] || unzip ".cache/awscliv2.zip" -d ".cache"
-  [ -e ".cache/aws-cli" ] || ".cache/aws/install" --install-dir "${PWD}/.cache/aws-cli" -b "${PWD}/bin"
+  [ -e "${PWD}/.cache/aws-cli" ] || {
+    [ -e .cache/aws ] || unzip ".cache/awscliv2.zip" -d ".cache"
+    ".cache/aws/install" --install-dir "${PWD}/.cache/aws-cli" -b "${PWD}/bin"
+  }
   [ ! -e ".cache/aws" ] || rm -rf ".cache/aws"
 }
 
@@ -61,7 +64,7 @@ function install_minikube {
   # Install minikube
   # https://minikube.sigs.k8s.io/docs/start/
   [ -e "bin/minikube" ] || {
-    echo "> Installing minikube"
+    msg_info "> Installing minikube"
     [ -e "bin" ] || mkdir "bin"
     curl -sLo "bin/minikube" "${minikube_url}"
     chmod a+x "bin/minikube"
@@ -74,6 +77,7 @@ function install_kubectl {
   # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 
   [ -e "bin/kubectl" ] || {
+    msg_info "> Installing kubectl"
     curl -sLo "bin/kubectl" "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod a+x "bin/kubectl"
     alias kubectl="$PWD/bin/kubectl"
@@ -83,6 +87,7 @@ function install_kubectl {
 
 function configure_minikube {
   # Configure minikube
+  msg_info "> Configuring minikube"
   minikube config set cpus "${minikube_cpus}"
   minikube config set memory "${minikube_memory}"
   minikube config set disk-size "${minikube_disk_size}"
@@ -91,7 +96,7 @@ function configure_minikube {
 
 function start_minikube {
   # Start minikube
-  echo "> Starting minikube"
+  msg_info "> Starting minikube"
   minikube start \
     --cpus "${minikube_cpus}" \
     --memory "${minikube_memory}" \
@@ -101,14 +106,17 @@ function start_minikube {
 
 function enable_minikube_addons {
   # Enable internal image registry at port 5000 in the minikube vm
+  msg_info "> Enable minikube addons"
   minikube addons enable registry
   minikube addons enable ingress
 }
 
 function install_clowder {
   # Install clowder
-  echo "> Install clowder"
-  curl -sLo .cache/kube_setup.sh https://raw.githubusercontent.com/RedHatInsights/clowder/master/build/kube_setup.sh
+  msg_info "> Installing clowder"
+  [ -e .cache/kube_setup.sh ] || {
+    curl -sLo .cache/kube_setup.sh https://raw.githubusercontent.com/RedHatInsights/clowder/master/build/kube_setup.sh
+  }
   chmod +x .cache/kube_setup.sh
   ./.cache/kube_setup.sh
   # Check on this page if this is the last version:
@@ -118,6 +126,7 @@ function install_clowder {
 
 function create_and_setup_namespace {
   # Create namespace and configure it
+  msg_info "> Creating and setup namespace '${namespace}'"
   kubectl get namespace "${namespace}" &>/dev/null \
   || kubectl create namespace "${namespace}"
   kubectl config set-context --current --namespace="${namespace}"
@@ -127,7 +136,7 @@ function create_and_setup_namespace {
 
 function install_python_dependencies {
   # Install python dependencies
-  echo "> Preparing .venv python environment"
+  msg_info "> Preparing .venv python environment"
   [ -e .venv ] || python -m venv .venv
   # shellcheck disable=SC1091
   source .venv/bin/activate
@@ -138,6 +147,7 @@ function install_python_dependencies {
 function setup_node_environment {
   # Preparing node environment by using nvm
   # https://github.com/nvm-sh/nvm
+  msg_info "> Setting up node environment"
   command -v nvm &>/dev/null || {
     curl -sL https://raw.githubusercontent.com/creationix/nvm/master/install.sh | env -i HOME="$HOME" bash
   }
@@ -161,17 +171,37 @@ function setup_node_environment {
 
 function deploy_environment {
   # Deploy a local environment
-  echo "> Deploying local environment with bonfire"
+  msg_info "> Deploying local environment with bonfire"
   bonfire deploy-env -n "${namespace}" -u "${quayio_user}"
   # bonfire deploy \
   #   --namespace "${namespace}"
   #   --source appsre \
-  #   --local-config-path "config/bonfire-config-local.yaml" \
+  #   --local-config-path "config/bonfire-local.yaml" \
   #   --clowd-env "env-${namespace}"
 }
 
+
+function clone_repo_to_directory {
+  local repo="$1"
+  local directory="$2"
+
+  [ "${repo}" != "" ] || {
+    msg_error "'repo' is empty"
+    return 1
+  }
+  [ "${directory}" != "" ] || {
+    msg_error "'directory' is empty"
+    return 1
+  }
+  [ -e "${directory}" ] || {
+    info_msg "> Cloning '${repo}' to '${directory}'"
+    git clone -o downstream "${repo}" "${directory}"
+  }
+}
+
+
 function download_external_repos {
-  echo "> Downloading necessary repositories?"
+  msg_info "> Downloading necessary repositories?"
 
   repos=()
   repos+=("https://github.com/RedHatInsights/frontend-components.git")
@@ -185,23 +215,57 @@ function download_external_repos {
   repos+=("https://github.com/RedHatInsights/insights-remediations.git")
   repos+=("https://github.com/RedHatInsights/remediations-consumer.git")
   repos+=("https://github.com/RedHatInsights/rbac-config.git")
-  repos+=("https://gitlab.cee.redhat.com/service/app-interface.git")
 
   for repo in "${repos[@]}"; do
     directory="${repo##*/}"
     directory="${directory%.git}"
     directory="external/${directory}"
-    [ -e "${directory}" ] || {
-      echo "> Cloning '${repo}' to '${directory}'"
-      git clone -o downstream "${repo}" "${directory}"
-    }
+    clone_repo_to_directory "${repo}" "${directory}"
   done
 }
 
+
+function download_appsre_repos {
+  # https://gitlab.cee.redhat.com/service/app-interface/-/blob/master/docs/app-sre/sop/app-interface-development-environment-setup.md#setup-repo
+  [ -e external/dev/app-sre ] || mkdir -p external/dev/app-sre
+  [ -e external/dev/service ] || mkdir -p external/dev/service
+
+  repos=()
+  repos+=("https://gitlab.cee.redhat.com/service/app-interface.git")
+  for r in "${repos[@]}"; do
+    directory="${r##*/}"
+    directory="${directory%.git}"
+    directory="external/dev/service/${directory}"
+    clone_repo_to_directory "${r}" "${directory}"
+  done
+  (cd external/dev/service/app-interface; git checkout Makefile; git apply ../../../../config/patches/dev/service/app-interface/0001-patch-makefile-dirs.patch)
+
+  repos=()
+  repos+=("https://github.com/app-sre/qontract-server.git")
+  repos+=("https://github.com/app-sre/qontract-reconcile.git")
+  repos+=("https://github.com/app-sre/qontract-schemas.git")
+  for r in "${repos[@]}"; do
+    directory="${r##*/}"
+    directory="${directory%.git}"
+    directory="external/dev/app-sre/${directory}"
+    clone_repo_to_directory "${r}" "${directory}"
+  done
+
+  [ -e "external/dev/service/app-interface/schema" ] || {
+    ln -svf "../../app-sre/qontract-schemas/schemas" "external/dev/service/app-interface/schemas"
+  }
+  [ -e "external/dev/service/app-interface/graphql-schemas" ] || {
+    ln -svf "../../app-sre/qontract-schemas/graphql-schemas" "external/dev/service/app-interface/graphql-schemas"
+  }
+}
+
+
 function install_podman {
   # Install podman if not yet
-  command -v podman &>/dev/null \
-  || sudo dnf install -y podman
+  command -v podman &>/dev/null || {
+    msg_info "> Installing podman"
+    sudo dnf install -y podman
+  }
 }
 
 # https://consoledot.pages.redhat.com/docs/dev/getting-started/local/development.html#_a_combined_local_build_sh
@@ -240,6 +304,10 @@ install_aws_cli
 install_minikube
 install_kubectl
 install_podman
+
+download_external_repos
+download_appsre_repos
+
 configure_minikube
 start_minikube
 enable_minikube_addons
@@ -249,7 +317,6 @@ install_python_dependencies
 setup_node_environment
 deploy_environment
 
-download_external_repos
 
 
 # Install an application by:
@@ -261,9 +328,8 @@ download_external_repos
 # Generate bonfire defaults
 # bonfire config write-default # By default it writes at ~/.config/bonfire/config.yaml
 # bonfire config write-default config/bonfire-config.yaml
-# INFO I do not know yet how to specify a specific configuration file
-# INFO I would like to have a self-contained environment, no other configuration file
-#      out of the current base directory.
+#
+# bonfire deploy --local-config-path config/bonfire-config-local.yaml -n "${namespace}"
 
 
 
@@ -280,7 +346,7 @@ download_external_repos
 APP=insights-idm
 export APP
 [ -e "external/${APP}-frontend" ] || {
-	echo "> Creating frontend application at 'external/${APP}'"
+	msg_info "> Creating frontend application at 'external/${APP}'"
   # FIXME 'npm install --save webpack-bundle-analyzer' is needed to do 'npm run build' and './node_modules/.bin/fec static'
 	(cd external && create-crc-app "${APP}-frontend" && cd "${APP}-frontend" && npm install --save webpack-bundle-analyzer)
   generate_local_build > "external/${APP}-frontend/local_build.sh"
@@ -306,4 +372,4 @@ EOF
 # To use the environment setup by this script, just do:
 # source config/prepare-env.sh
 
-# bonfire deploy --local-config-path config/bonfire-config-local.yaml
+# bonfire deploy --local-config-path config/bonfire-local.yaml -n "${namespace}"
