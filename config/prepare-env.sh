ca2014bd7c4ce4d2952e4741f69ecc65b1c7e7f4
Alejandro Visiedo <avisiedo@redhat.com>
2022-04-21 11:35:20 +0200

    Set env vars to point out the AWS configuration

--- config/prepare-env.sh
+++ config/prepare-env.sh
@@ -7,3 +7,5 @@ source <(kubectl completion bash)
 complete -C "${PWD}/bin/aws_completer" aws
 export SCHEMAS_DIR="${PWD}/external/dev/service/app-sre/qontract-schemas/schemas"
 export GRAPHQL_SCHEMAS_DIR="${PWD}/external/dev/app-sre/qontract-schemas/graphql-schemas"
+[ ! -e "$PWD/secrets/aws_credentials" ] || export AWS_SHARED_CREDENTIALS_FILE="$PWD/secrets/aws_credentials"
+[ ! -e "$PWD/secrets/aws_config" ] || export AWS_CONFIG_FILE="$PWD/secrets/aws_config"
